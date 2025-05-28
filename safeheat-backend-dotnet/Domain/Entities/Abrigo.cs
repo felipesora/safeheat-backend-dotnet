f2cee89253b0ec95c5c24716366b238cd1b875b2
felipesora <felipeusora@gmail.com>
2025-05-28 13:53:27 -0300

    Criação das entidades

---
+++ safeheat-backend-dotnet/Domain/Entities/Abrigo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace safeheat_backend_dotnet.Domain.Entities;
+public class Abrigo
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Nome { get; set; }
+
+    [Required]
+    public string Endereco { get; set; }
+
+    [Required]
+    public string Cidade { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int CapacidadeTotal { get; set; }
+
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int CapacidadeAtual { get; set; }
+
+    // Relacionamento 1:N
+    public ICollection<RegistroDeEntrada> RegistrosDeEntrada { get; set; }
+}
---
+++ safeheat-backend-dotnet/Domain/Entities/RegistroDeEntrada.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace safeheat_backend_dotnet.Domain.Entities;
+
+public class RegistroDeEntrada
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string NomePessoa { get; set; }
+
+    public DateTime DataEntrada { get; set; } = DateTime.Now;
+
+    // Chave estrangeira
+    public int AbrigoId { get; set; }
+
+    // Propriedade de navegação
+    [ForeignKey("AbrigoId")]
+    public Abrigo Abrigo { get; set; }
+}
