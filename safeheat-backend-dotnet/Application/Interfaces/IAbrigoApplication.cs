f4c853a16f641df8ddf8ad07620acde8af49a037
felipesora <felipeusora@gmail.com>
2025-05-28 16:03:39 -0300

    Criação das interfaces para as Services

---
+++ safeheat-backend-dotnet/Application/Interfaces/IAbrigoApplication.cs
@@ -0,0 +1,13 @@
+﻿using safeheat_backend_dotnet.Application.DTOs;
+using safeheat_backend_dotnet.Domain.Entities;
+
+namespace safeheat_backend_dotnet.Application.Interfaces;
+
+public interface IAbrigoApplication
+{
+    IEnumerable<AbrigoEntity>? ObterTodos();
+    AbrigoEntity? ObterPorId(int id);
+    AbrigoEntity? Salvar(AbrigoDto abrigo);
+    AbrigoEntity? Editar(int id, AbrigoDto abrigo);
+    AbrigoEntity? Deletar(int id);
+}
---
+++ safeheat-backend-dotnet/Application/Interfaces/IRegistroDeEntradaApplication.cs
@@ -0,0 +1,13 @@
+﻿using safeheat_backend_dotnet.Application.Dtos;
+using safeheat_backend_dotnet.Domain.Entities;
+
+namespace safeheat_backend_dotnet.Application.Interfaces;
+
+public interface IRegistroDeEntradaApplication
+{
+    IEnumerable<RegistroDeEntradaEntity>? ObterTodos();
+    RegistroDeEntradaEntity? ObterPorId(int id);
+    RegistroDeEntradaEntity? Salvar(RegistroDeEntradaDto registro);
+    RegistroDeEntradaEntity? Editar(int id, RegistroDeEntradaDto registro);
+    RegistroDeEntradaEntity? Deletar(int id);
+}
