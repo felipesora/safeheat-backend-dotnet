14fe0d582150e4372915177654ce38e317508807
felipesora <felipeusora@gmail.com>
2025-05-28 14:14:06 -0300

    Criação das interfaces dos repositories

---
+++ safeheat-backend-dotnet/Domain/Interfaces/IAbrigoRepository.cs
@@ -0,0 +1,12 @@
+﻿using safeheat_backend_dotnet.Domain.Entities;
+
+namespace safeheat_backend_dotnet.Domain.Interfaces;
+
+public interface IAbrigoRepository
+{
+    IEnumerable<Abrigo>? ObterTodos();
+    Abrigo? ObterPorId(int id);
+    Abrigo? Salvar(Abrigo abrigo);
+    Abrigo? Editar(int id, Abrigo abrigo);
+    Abrigo? Deletar(int id);
+}
---
+++ safeheat-backend-dotnet/Domain/Interfaces/IRegistroDeEntradaRepository.cs
@@ -0,0 +1,12 @@
+﻿using safeheat_backend_dotnet.Domain.Entities;
+
+namespace safeheat_backend_dotnet.Domain.Interfaces;
+
+public interface IRegistroDeEntradaRepository
+{
+    IEnumerable<RegistroDeEntrada>? ObterTodos();
+    RegistroDeEntrada? ObterPorId(int id);
+    RegistroDeEntrada? Salvar(RegistroDeEntrada registro);
+    RegistroDeEntrada? Editar(int id, RegistroDeEntrada registro);
+    RegistroDeEntrada? Deletar(int id);
+}
