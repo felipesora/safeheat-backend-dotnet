7097117ce222287b7128638717afc18935fd0ed6
felipesora <felipeusora@gmail.com>
2025-05-28 19:56:23 -0300

    Criação das Services

---
+++ safeheat-backend-dotnet/Application/Services/AbrigoApplication.cs
@@ -0,0 +1,62 @@
+﻿using Humanizer;
+using safeheat_backend_dotnet.Application.DTOs;
+using safeheat_backend_dotnet.Application.Interfaces;
+using safeheat_backend_dotnet.Domain.Entities;
+using safeheat_backend_dotnet.Domain.Interfaces;
+
+namespace safeheat_backend_dotnet.Application.Services;
+
+public class AbrigoApplication : IAbrigoApplication
+{
+    private readonly IAbrigoRepository _abrigoApplication;
+
+    public AbrigoApplication(IAbrigoRepository abrigoApplication)
+    {
+        _abrigoApplication = abrigoApplication;
+    }
+
+    public IEnumerable<AbrigoEntity>? ObterTodos()
+    {
+        return _abrigoApplication.ObterTodos() ?? Enumerable.Empty<AbrigoEntity>();
+    }
+
+    public AbrigoEntity? ObterPorId(int id)
+    {
+        return _abrigoApplication.ObterPorId(id);
+    }
+
+    public AbrigoEntity? Salvar(AbrigoDto dto)
+    {
+        var abrigo = new AbrigoEntity
+        {
+            Nome = dto.Nome,
+            Endereco = dto.Endereco,
+            Cidade = dto.Cidade,
+            CapacidadeTotal = dto.CapacidadeTotal,
+            CapacidadeAtual = dto.CapacidadeAtual
+        };
+
+        return _abrigoApplication.Salvar(abrigo);
+    }
+
+    public AbrigoEntity? Editar(int id, AbrigoDto dto)
+    {
+        var abrigo = new AbrigoEntity
+        {
+            Id = id,
+            Nome = dto.Nome,
+            Endereco = dto.Endereco,
+            Cidade = dto.Cidade,
+            CapacidadeTotal = dto.CapacidadeTotal,
+            CapacidadeAtual = dto.CapacidadeAtual
+        };
+
+        return _abrigoApplication.Editar(id, abrigo);
+    }
+
+    public AbrigoEntity? Deletar(int id)
+    {
+        return _abrigoApplication.Deletar(id);
+    }
+
+}
---
+++ safeheat-backend-dotnet/Application/Services/RegistroDeEntradaApplication.cs
@@ -0,0 +1,55 @@
+﻿using safeheat_backend_dotnet.Application.Dtos;
+using safeheat_backend_dotnet.Application.Interfaces;
+using safeheat_backend_dotnet.Domain.Entities;
+using safeheat_backend_dotnet.Domain.Interfaces;
+
+namespace safeheat_backend_dotnet.Application.Services;
+
+public class RegistroDeEntradaApplication : IRegistroDeEntradaApplication
+{
+    private readonly IRegistroDeEntradaRepository _registroApplication;
+
+    public RegistroDeEntradaApplication(IRegistroDeEntradaRepository registroApplication)
+    {
+        _registroApplication = registroApplication;
+    }
+
+    public IEnumerable<RegistroDeEntradaEntity>? ObterTodos()
+    {
+        return _registroApplication.ObterTodos() ?? Enumerable.Empty<RegistroDeEntradaEntity>();
+    }
+
+    public RegistroDeEntradaEntity? ObterPorId(int id)
+    {
+        return _registroApplication.ObterPorId(id);
+    }
+
+    public RegistroDeEntradaEntity? Salvar(RegistroDeEntradaDto dto)
+    {
+        var registro = new RegistroDeEntradaEntity
+        {
+            NomePessoa = dto.NomePessoa,
+            AbrigoId = dto.AbrigoId
+        };
+
+        return _registroApplication.Salvar(registro);
+    }
+
+    public RegistroDeEntradaEntity? Editar(int id, RegistroDeEntradaDto dto)
+    {
+        var registro = new RegistroDeEntradaEntity
+        {
+            Id = id,
+            NomePessoa = dto.NomePessoa,
+            AbrigoId = dto.AbrigoId
+        };
+
+        return _registroApplication.Editar(id, registro);
+    }
+
+    public RegistroDeEntradaEntity? Deletar(int id)
+    {
+        return _registroApplication.Deletar(id);
+    }
+
+}
