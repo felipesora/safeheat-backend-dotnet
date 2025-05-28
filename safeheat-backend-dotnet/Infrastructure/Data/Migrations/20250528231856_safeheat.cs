0e0534c999aed296c3d4b579bc897a8340ac1577
felipesora <felipeusora@gmail.com>
2025-05-28 20:37:23 -0300

    Criação das Migrations

---
+++ safeheat-backend-dotnet/Infrastructure/Data/Migrations/20250528231856_safeheat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace safeheat_backend_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class safeheat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SH_ABRIGOS",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Endereco = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Cidade = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CapacidadeTotal = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    CapacidadeAtual = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SH_ABRIGOS", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SH_REGISTOS_ENTRADA",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    NomePessoa = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    DataEntrada = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    AbrigoId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SH_REGISTOS_ENTRADA", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SH_REGISTOS_ENTRADA_SH_ABRIGOS_AbrigoId",
+                        column: x => x.AbrigoId,
+                        principalTable: "SH_ABRIGOS",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SH_REGISTOS_ENTRADA_AbrigoId",
+                table: "SH_REGISTOS_ENTRADA",
+                column: "AbrigoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SH_REGISTOS_ENTRADA");
+
+            migrationBuilder.DropTable(
+                name: "SH_ABRIGOS");
+        }
+    }
+}
--- safeheat-backend-dotnet/Program.cs
+++ safeheat-backend-dotnet/Program.cs
@@ -1,4 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using safeheat_backend_dotnet.Application.Interfaces;
+using safeheat_backend_dotnet.Application.Services;
+using safeheat_backend_dotnet.Domain.Interfaces;
+using safeheat_backend_dotnet.Infrastructure.Data.AppData;
+using safeheat_backend_dotnet.Infrastructure.Data.Repositores;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationContext>(options => {
+    options.UseOracle(builder.Configuration.GetConnectionString("Oracle"));
+});
+
+builder.Services.AddTransient<IAbrigoRepository, AbrigoRepository>();
+builder.Services.AddTransient<IAbrigoApplication, AbrigoApplication>();
+builder.Services.AddTransient<IRegistroDeEntradaRepository, RegistroDeEntradaRepository>();
+builder.Services.AddTransient<IRegistroDeEntradaApplication, RegistroDeEntradaApplication>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
