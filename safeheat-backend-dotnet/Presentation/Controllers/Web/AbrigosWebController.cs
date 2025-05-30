bf73988ed5f4fa026f3ec325cc6ef6fc1a36993e
felipesora <felipeusora@gmail.com>
2025-05-29 23:56:29 -0300

    Criação dos Controller WEB e Views

---
+++ safeheat-backend-dotnet/Presentation/Controllers/Web/AbrigosWebController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using safeheat_backend_dotnet.Application.DTOs;
+using safeheat_backend_dotnet.Application.Interfaces;
+
+namespace safeheat_backend_dotnet.Presentation.Controllers.Web;
+
+public class AbrigosWebController : Controller
+{
+    private readonly IAbrigoApplication _abrigoApplication;
+
+    public AbrigosWebController(IAbrigoApplication abrigoApplication)
+    {
+        _abrigoApplication = abrigoApplication;
+    }
+
+    // Listar todos os abrigos
+    public IActionResult Index()
+    {
+        var abrigos = _abrigoApplication.ObterTodos();
+        return View(abrigos);
+    }
+
+    // Tela de cadastro de abrigo (GET)
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // Cadastro do abrigo (POST)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(AbrigoDto dto)
+    {
+        if (!ModelState.IsValid)
+            return View(dto);
+
+        _abrigoApplication.Salvar(dto);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // Tela de edição (GET)
+    public IActionResult Edit(int id)
+    {
+        var abrigo = _abrigoApplication.ObterPorId(id);
+        if (abrigo == null) return NotFound();
+
+        var dto = new AbrigoDto
+        {
+            Nome = abrigo.Nome,
+            Endereco = abrigo.Endereco,
+            Cidade = abrigo.Cidade,
+            CapacidadeTotal = abrigo.CapacidadeTotal,
+            CapacidadeAtual = abrigo.CapacidadeAtual
+        };
+
+        return View(dto);
+    }
+
+    // Edição do abrigo (POST)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, AbrigoDto dto)
+    {
+        if (!ModelState.IsValid)
+            return View(dto);
+
+        _abrigoApplication.Editar(id, dto);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // Deletar abrigo
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id)
+    {
+        _abrigoApplication.Deletar(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+    // Ver recursos do abrigo
+    public IActionResult Recursos(int abrigoId)
+    {
+        return RedirectToAction("Index", "RecursosDisponiveisWeb", new { abrigoId });
+    }
+}
---
+++ safeheat-backend-dotnet/Presentation/Controllers/Web/RecursosDisponiveisWebController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using safeheat_backend_dotnet.Application.Dtos;
+using safeheat_backend_dotnet.Application.Interfaces;
+
+namespace safeheat_backend_dotnet.Presentation.Controllers.Web;
+
+public class RecursosDisponiveisWebController : Controller
+{
+    private readonly IRecursoDisponivelApplication _recursoApplication;
+
+    public RecursosDisponiveisWebController(IRecursoDisponivelApplication recursoApplication)
+    {
+        _recursoApplication = recursoApplication;
+    }
+
+    // Listar recursos de um abrigo
+    public IActionResult Index(int abrigoId)
+    {
+        var recursos = _recursoApplication.ObterTodos()
+            .Where(r => r.AbrigoId == abrigoId)
+            .ToList();
+
+        ViewBag.AbrigoId = abrigoId;
+        return View(recursos);
+    }
+
+    // Tela de cadastro do recurso (GET)
+    public IActionResult Create(int abrigoId)
+    {
+        var dto = new RecursoDisponivelDto { AbrigoId = abrigoId };
+        return View(dto);
+    }
+
+    // Cadastro do recurso (POST)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(RecursoDisponivelDto dto)
+    {
+        if (!ModelState.IsValid)
+            return View(dto);
+
+        _recursoApplication.Salvar(dto);
+        return RedirectToAction(nameof(Index), new { abrigoId = dto.AbrigoId });
+    }
+
+    // Tela de edição (GET)
+    public IActionResult Edit(int id)
+    {
+        var recurso = _recursoApplication.ObterPorId(id);
+        if (recurso == null) return NotFound();
+
+        var dto = new RecursoDisponivelDto
+        {
+            Nome = recurso.Nome,
+            Quantidade = recurso.Quantidade,
+            AbrigoId = recurso.AbrigoId
+        };
+
+        ViewBag.Id = id;
+        return View(dto);
+    }
+
+    // Edição do recurso (POST)
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, RecursoDisponivelDto dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            ViewBag.Id = id;
+            return View(dto);
+        }
+
+        _recursoApplication.Editar(id, dto);
+        return RedirectToAction(nameof(Index), new { abrigoId = dto.AbrigoId });
+    }
+
+    // Deletar recurso
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id, int abrigoId)
+    {
+        _recursoApplication.Deletar(id);
+        return RedirectToAction(nameof(Index), new { abrigoId });
+    }
+}
